2305f1bd1e9f2aa7c3bb287350799dd5050440dc
Huds0n <32389322+Huds0n@users.noreply.github.com>
2018-12-06 15:17:45 +1100

    Blocking Prototype Started

---
+++ Millennial Nightmare/Assets/Scripts/Character.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+    public float moveSpeed = 2f;
+	void Start () {
+		
+	}
+
+	void Update () {
+        //Forwards and back mvoement
+        transform.Translate(Vector3.forward * Time.deltaTime * Input.GetAxis("Vertical") * moveSpeed);
+        //Left and right movement
+        transform.Translate(Vector3.right * Time.deltaTime * Input.GetAxis("Horizontal") * moveSpeed);
+    }
+}
---
+++ Millennial Nightmare/Assets/Scripts/MainCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour {
+    public float rotateSpeed = 1f;
+    public Transform target;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("space")){
+            transform.LookAt(target);
+            print("looking up");
+        }
+	}
+}
