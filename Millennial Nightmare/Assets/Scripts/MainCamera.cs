ef377cf9dfa8f21567e769a00c5eaa43f295678d
J-Marshall1997 <jamesdm6@gmail.com>
2018-12-12 10:36:57 +1100

    Smoothed out Looking Up

--- Millennial Nightmare/Assets/Scripts/MainCamera.cs
+++ Millennial Nightmare/Assets/Scripts/MainCamera.cs
@@ -3,23 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MainCamera : MonoBehaviour {
-    public float rotateSpeed = 1f;
+    public float rotateSpeed = 10f;
     public Transform forwardTarget;
     public Transform phone;
+    private bool isLookingUp = false;
 
 	void Start () {
         //transform.LookAt(phone);
     }
 	void Update () {
         if (Input.GetKeyDown("space")){
-            LookUp();  
+            isLookingUp = true; 
+        }
+        if (isLookingUp) {
+            LookUp();
         }
 	}
 
     void LookUp(){
         //transform.LookAt(forward);
         Vector3 targetDir = forwardTarget.position - transform.position;
-        Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, rotateSpeed, 0f);
+        Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, rotateSpeed * Time.deltaTime, 0f);
         transform.rotation = Quaternion.LookRotation(newDir);
         print("looking up");
         StartCoroutine(Wait());
@@ -30,8 +34,9 @@ public class MainCamera : MonoBehaviour {
     IEnumerator Wait(){
         yield return new WaitForSeconds(3);
         Vector3 targetDir = phone.position - transform.position;
-        Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, rotateSpeed, 0f);
+        Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, rotateSpeed * Time.deltaTime, 0f);
         transform.rotation = Quaternion.LookRotation(newDir);
+        isLookingUp = false;
         //transform.LookAt(phone);
     }
 }
