1f293a0121ed2e4b8ac6477a98c51ffb17ba611b
J-Marshall1997 <jamesdm6@gmail.com>
2019-01-24 12:20:31 +1100

    Creation on Colour Minigame
    Colour minigame chooses 1 of 4 colours at random and prints a set amount. The player will have to memorise these colours and enter them at the end of the level.

---
+++ Millennial Nightmare/Assets/Scripts/MiniGames/ColourGame.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourGame : MonoBehaviour {
+
+    //Colours
+    public string[] colours = { "Red", "Green", "Blue", "Yellow" };
+    //random number generator
+    private static readonly System.Random getrandom = new System.Random();
+    //num of colours to print
+    private int numColours;
+
+	// Use this for initialization
+	void Start () {
+        numColours = 5;
+        InvokeRepeating("ChooseColour", 0.0f, 3.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //Chooses a random colour and prints to console
+    void ChooseColour() {
+        int colourNumber = 6 - numColours;
+        int index = getrandom.Next(4);
+        print("Colour Number " + colourNumber.ToString() + ": "+ colours[index]);
+        //After set number of colours, stop choosing more
+        if (--numColours == 0) {
+            CancelInvoke("ChooseColour");
+            print("No more colours");
+        }
+    }
+}
