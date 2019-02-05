4b28660a9015a8d7f2ad7ebef1c2632133c36cf6
J-Marshall1997 <jamesdm6@gmail.com>
2019-02-05 16:40:17 +1100

    Spawn Objects in unity no longer exist, are in code instead

--- Millennial Nightmare/Assets/Scripts/EnemySpawner.cs
+++ Millennial Nightmare/Assets/Scripts/EnemySpawner.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour {
 
-    public Transform[] spawners;
+    public ArrayList spawners;
     public GameObject[] enemy;
     public GameObject[] whatToSpawn;
     private bool whenToSpawn = true;
 
     void Start () {
+        spawners = new ArrayList();
+        CreateSpawners();
         SpawnEnemyPerson();
 	}
 
@@ -18,13 +20,31 @@ public class EnemySpawner : MonoBehaviour {
 	}
     //Spawning which enemy ay which location and rotation
     void SpawnEnemyPerson() {
-        whatToSpawn[1] = Instantiate(enemy[1],spawners[0].transform.position, Quaternion.Euler(0, 90, 0))as GameObject;
-        whatToSpawn[1] = Instantiate(enemy[1], spawners[1].transform.position, Quaternion.Euler(0, -90, 0)) as GameObject;
-        whatToSpawn[1] = Instantiate(enemy[1], spawners[2].transform.position, Quaternion.Euler(0, 180, 0)) as GameObject;
-        whatToSpawn[0] = Instantiate(enemy[0], spawners[3].transform.position, Quaternion.Euler(0, 90, 0)) as GameObject;
-        whatToSpawn[0] = Instantiate(enemy[0], spawners[4].transform.position, Quaternion.Euler(0, -90, 0)) as GameObject;
-        whatToSpawn[0] = Instantiate(enemy[0], spawners[5].transform.position, Quaternion.Euler(0, 180, 0)) as GameObject;
-        whatToSpawn[0] = Instantiate(enemy[0], spawners[6].transform.position, Quaternion.Euler(0, 0, 0)) as GameObject;
+        whatToSpawn[1] = Instantiate(enemy[1], ((Spawner)spawners[0]).position, ((Spawner)spawners[0]).orientation) as GameObject;
+        whatToSpawn[1] = Instantiate(enemy[1], ((Spawner)spawners[1]).position, ((Spawner)spawners[1]).orientation) as GameObject;
+        whatToSpawn[1] = Instantiate(enemy[1], ((Spawner)spawners[2]).position, ((Spawner)spawners[2]).orientation) as GameObject;
+        whatToSpawn[0] = Instantiate(enemy[0], ((Spawner)spawners[3]).position, ((Spawner)spawners[3]).orientation) as GameObject;
+        whatToSpawn[0] = Instantiate(enemy[0], ((Spawner)spawners[4]).position, ((Spawner)spawners[4]).orientation) as GameObject;
+        whatToSpawn[0] = Instantiate(enemy[0], ((Spawner)spawners[5]).position, ((Spawner)spawners[5]).orientation) as GameObject;
+        whatToSpawn[0] = Instantiate(enemy[0], ((Spawner)spawners[6]).position, ((Spawner)spawners[6]).orientation) as GameObject;
         
+    }
+
+    private void CreateSpawners() {
+        Spawner spawner1 = new Spawner(-55f, 1.35f, 44.5f, Quaternion.Euler(0, 90, 0), "person"); //Works
+        Spawner spawner2 = new Spawner(55f, 1.35f, 44f, Quaternion.Euler(0, -90, 0), "person");
+        Spawner spawner3 = new Spawner(-1.43f, 1.35f, 168.5f, Quaternion.Euler(0, 180, 0), "person");
+        Spawner spawner4 = new Spawner(-52.75f, -0.33f, 55.25f, Quaternion.Euler(0, 90, 0), "car");
+        Spawner spawner5 = new Spawner(55.5f, -0.33f, 50.5f, Quaternion.Euler(0, -90, 0), "car");
+        Spawner spawner6 = new Spawner(3f, -0.33f, 164f, Quaternion.Euler(0, 180, 0), "car");
+        Spawner spawner7 = new Spawner(-1.5f, -0.33f, -25f, Quaternion.Euler(0, 0, 0), "car");
+
+        spawners.Add(spawner1);
+        spawners.Add(spawner2);
+        spawners.Add(spawner3);
+        spawners.Add(spawner4);
+        spawners.Add(spawner5);
+        spawners.Add(spawner6);
+        spawners.Add(spawner7);
     }
 }
---
+++ Millennial Nightmare/Assets/Scripts/Spawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner {
+
+    public Vector3 position;
+    public Quaternion orientation;
+    private string enemyType;
+
+	// Use this for initialization
+	public Spawner(float x, float y, float z, Quaternion omega, string enemy) {
+        position = new Vector3(x, y, z);
+        orientation = omega;
+        enemyType = enemy;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
